8982a4f96ef349ca6d73d586b9c18e0de4158131
doodi <dt192@bgt.school>
2025-10-13 14:22:57 +0200

    Created Task1.dart

--- Day1/Task1.dart
+++ Day1/Task1.dart
@@ -2,22 +2,19 @@ void main() {
   List<double> nota = [7, 8.5, 10, 6];
   double rezultat = mesatarja(nota);
 
-  // Printo mesataren me 2 shifra pas presjes
   print(rezultat.toStringAsFixed(2));
 }
 
 double mesatarja(List<double> nota) {
-  // Nëse lista është bosh → kthe 0.0
   if (nota.isEmpty) {
     return 0.0;
   }
 
-  // Llogarit shumën e notave
   double shuma = 0.0;
   for (double n in nota) {
     shuma += n;
   }
 
-  // Kthe mesataren
+  // Outputi
   return shuma / nota.length;
 }
