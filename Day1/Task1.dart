9f4c96a5e052fb24e790daee68ee0d10eea353ac
Gjin901 <gs110@bgt.school>
2025-10-13 15:21:36 +0200

    Update Task1.dart

--- Day1/Task1.dart
+++ Day1/Task1.dart
@@ -1,4 +1,4 @@
-
+Task1
 import 'dart:io';
 
 double mesatarja(List<double> nota) {
@@ -20,3 +20,30 @@ void main() {
  
   print(rezultati.toStringAsFixed(2));
 }
+
+
+Task 2 
+  Map<String, List<int>> ndaj(List<int> nums) {
+  List<int> cift = [];
+  List<int> tek = [];
+
+  for (var n in nums) {
+    if (n % 2 == 0) {
+      cift.add(n);
+    } else {
+      tek.add(n);
+    }
+  }
+
+  return {'cift': cift, 'tek': tek};
+}
+
+void main() {
+  List<int> numrat = [1, 2, 3, 4, 5, 6];
+
+  var rezultati = ndaj(numrat);
+
+  print('cift: ${rezultati['cift']}');
+  print('tek: ${rezultati['tek']}');
+}
+ 
