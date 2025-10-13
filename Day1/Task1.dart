a51131fe5b9bd1d8d7d92fd2ee28922baf0cd982
erti787 <es196@bgt.school>
2025-10-13 14:54:40 +0200

    Task1.dart

--- Day1/Task1.dart
+++ Day1/Task1.dart
@@ -1,1 +1,21 @@
+import 'dart:io';
 
+double mesatarja(List<double> nota) {
+  if (nota.isEmpty) return 0.0;
+
+  double shuma = 0.0;
+  for (var n in nota) {
+    shuma += n;
+  }
+  return shuma / nota.length;
+}
+
+void main() {
+
+  List<double> notat = [7, 8.5, 10, 6];
+
+  double rezultati = mesatarja(notat);
+
+ 
+  print(rezultati.toStringAsFixed(2));
+}
