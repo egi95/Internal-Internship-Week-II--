25e544bd025522ced1642c33cf2baee0ed9ede5d
YllBellopoja-YB <yb157@bgt.school>
2025-10-14 14:40:24 +0200

    Create Task1.dart

---
+++ Day2/Task1.dart
@@ -0,0 +1,30 @@
+class Student {
+  String emri;
+  List<double> nota;
+
+  Student(this.emri, this.nota);
+
+  double mesatarja() {
+    if (nota.isEmpty) return 0.0;
+    double total = nota.reduce((a, b) => a + b);
+    return total / nota.length;
+  }
+}
+
+void main() {
+  List<Student> studentet = [
+    Student("Ana", [8, 9, 10]),
+    Student("Ardit", [6, 7, 8]),
+    Student("Besa", []), // lista bosh
+  ];
+
+  Student topStudent = studentet[0];
+
+  for (var s in studentet) {
+    if (s.mesatarja() > topStudent.mesatarja()) {
+      topStudent = s;
+    }
+  }
+
+  print("Top: ${topStudent.emri} - ${topStudent.mesatarja().toStringAsFixed(2)}");
+}
