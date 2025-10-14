baccba3759f891cb89dc0fe4845e5d9f7c0abdd1
aron1i <ar137@bgt.school>
2025-10-14 22:28:36 +0200

    Update Task1.dart

--- Day1/Task1.dart
+++ Day1/Task1.dart
@@ -1,1 +1,15 @@
+void main() {
+  List<double> nota = [7, 8.5, 10, 6];
+
+  if (nota.isEmpty) {
+    print("0.00");
+  } else {
+    double shuma = 0;
+    for (double n in nota) {
+      shuma += n;
+    }
+    double mes = shuma / nota.length;
+    print(mes.toStringAsFixed(2));
+  }
+}
 
