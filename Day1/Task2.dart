e580f8700d13ca8a63d932d294337ad84d7be275
LekeB-crypto <lb147@bgt.school>
2025-10-14 15:05:23 +0200

    Update Task2.dart

--- Day1/Task2.dart
+++ Day1/Task2.dart
@@ -1,1 +1,31 @@
+void main() {
+  List<int> nums1 = [1, 2, 3, 4, 5, 6];
+  List<int> nums2 = [];
+  List<int> nums3 = [1, 3, 5];
+  List<int> nums4 = [2, 4, 8, 10];
 
+  print('Test 1: ${ndaj(nums1)}');
+  print('Test 2: ${ndaj(nums2)}');
+  print('Test 3: ${ndaj(nums3)}');
+  print('Test 4: ${ndaj(nums4)}');
+}
+
+Map<String, List<int>> ndaj(List<int> nums) {
+  List<int> cifta = [];
+  List<int> teka = [];
+
+  for (int n in nums) {
+    if (n % 2 == 0) {
+      
+      cifta.add(n);
+    } else {
+    
+      teka.add(n);
+    }
+  }
+
+  return {
+    'cift': cifta,
+    'tek': teka,
+  };
+}
