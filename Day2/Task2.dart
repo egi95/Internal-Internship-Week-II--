39e96afe489a2b5e995129f18f2772ddd9190818
alena-blakaj <ab120@bgt.school>
2025-10-14 22:14:08 +0200

    Update Task2.dart

--- Day2/Task2.dart
+++ Day2/Task2.dart
@@ -3,26 +3,25 @@ class Product {
   String name;
   double price;
 
-  // Konstruktor
+
   Product(this.id, this.name, this.price);
 
-  // Metodë për printim të bukur
+
   void info() {
     print("ID: $id | Emri: $name | Çmimi: €${price.toStringAsFixed(2)}");
   }
 }
 
-// Repo në memorie
+
 class Store {
   List<Product> store = [];
 
-  // Shton një produkt në listë
   void addProduct(Product p) {
     store.add(p);
     print("Produkti '${p.name}' u shtua me sukses!");
   }
 
-  // Fshin produktin sipas ID-së
+
   bool removeById(int id) {
     for (var p in store) {
       if (p.id == id) {
@@ -35,7 +34,7 @@ class Store {
     return false;
   }
 
-  // Kthen listën e produkteve në një interval çmimesh
+
   List<Product> inPriceRange(double min, double max) {
     List<Product> result = [];
     for (var p in store) {
@@ -46,9 +45,9 @@ class Store {
     return result;
   }
 
-  // Printon të gjitha produktet
+
   void showAll() {
-    print("\n📦 Produktet aktuale në dyqan:");
+    print("\n Produktet aktuale në dyqan:");
     for (var p in store) {
       p.info();
     }
@@ -56,23 +55,23 @@ class Store {
 }
 
 void main() {
-  // Krijojmë objektin "Store"
+
   var myStore = Store();
 
-  // Shtojmë disa produkte
+
   myStore.addProduct(Product(1, "Laptop", 899.99));
   myStore.addProduct(Product(2, "Mouse", 25.50));
   myStore.addProduct(Product(3, "Keyboard", 49.90));
   myStore.addProduct(Product(4, "Monitor", 220.00));
 
-  // Shfaqim të gjitha produktet
+ 
   myStore.showAll();
 
-  // Fshijmë një produkt sipas ID
+
   myStore.removeById(2);
 
-  // Gjejmë produktet me çmim nga 50 deri në 300 euro
-  print("\n💰 Produktet me çmim 50€ - 300€:");
+
+  print("\n Produktet me çmim 50€ - 300€:");
   var range = myStore.inPriceRange(50, 300);
   for (var p in range) {
     p.info();
