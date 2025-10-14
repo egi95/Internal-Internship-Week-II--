45e24233d2f57b1119fd2f5cf172710cdb364e19
B <eg167@bgt.school>
2025-10-14 20:47:04 +0200

    Update Task1.dart

--- Day2/Task1.dart
+++ Day2/Task1.dart
@@ -1,22 +1,13 @@
 void main() {
-  List<int> nums = [1, 2, 3, 4, 5, 6];
-  Map<String, List<int>> rezultati = ndaj(nums);
-
-  print("cift: ${rezultati['cift']}");
-  print("tek: ${rezultati['tek']}");
+  List<double> nota = [7, 8.5, 10, 6];
+  print(mesatarja(nota).toStringAsFixed(2));
 }
 
-Map<String, List<int>> ndaj(List<int> nums) {
-  List<int> cift = [];
-  List<int> tek = [];
-
-  for (var n in nums) {
-    if (n % 2 == 0) {
-      cift.add(n);
-    } else {
-      tek.add(n);
-    }
+double mesatarja(List<double> nota) {
+  if (nota.isEmpty) return 0.0;
+  double shuma = 0;
+  for (var n in nota) {
+    shuma += n;
   }
-
-  return {'cift': cift, 'tek': tek};
+  return shuma / nota.length;
 }
