1d18db9d912f04366b6cfcc96e71b80bd37cfe12
aron1i <ar137@bgt.school>
2025-10-14 22:32:33 +0200

    Update Task2.dart

--- Day1/Task2.dart
+++ Day1/Task2.dart
@@ -1,1 +1,21 @@
+void main() {
+  List<int> numrat = [1, 2, 3, 4, 5, 6];
 
+  List<int> cift = [];
+  List<int> tek = [];
+
+  if (numrat.isEmpty) {
+    print("Lista është e zbrazet");
+  } else {
+    for (int n in numrat) {
+      if (n % 2 == 0) {
+        cift.add(n);
+      } else {
+        tek.add(n);
+      }
+    }
+
+    print("cift: $cift");
+    print("tek: $tek");
+  }
+}
