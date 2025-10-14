6328642626c84b9eb1361afc69a7e6485b5e4c8f
Erioni10 <ea111@bgt.school>
2025-10-14 15:28:36 +0200

    Update Task2.dart

--- Day2/Task2.dart
+++ Day2/Task2.dart
@@ -1,25 +1,39 @@
-void main() {
-  List<int> numrat = [1, 2, 3, 4, 5, 6];
-  Map<String, List<int>> rezultat = ndaj(numrat);
+class Product {
+  int id;
+  String name;
+  double price;
 
-  print("cift: ${rezultat['cift']}");
-  print("tek: ${rezultat['tek']}");
+  Product(this.id, this.name, this.price);
 }
 
-Map<String, List<int>> ndaj(List<int> nums) {
-  List<int> cift = [];
-  List<int> tek = [];
+class Store {
+  List<Product> store = [];
 
-  for (int n in nums) {
-    if (n % 2 == 0) {
-      cift.add(n);
-    } else {
-      tek.add(n);
-    }
+  void addProduct(Product p) {
+    store.add(p);
   }
 
-  return {
-    'cift': cift,
-    'tek': tek,
-  };
+  bool removeById(int id) {
+    return store.removeWhere((p) => p.id == id) > 0;
+  }
+
+  List<Product> inPriceRange(double min, double max) {
+    return store.where((p) => p.price >= min && p.price <= max).toList();
+  }
+}
+
+void main() {
+  Store s = Store();
+
+  s.addProduct(Product(1, "Qumesht", 12.5));
+  s.addProduct(Product(2, "Djath", 8.0));
+  s.addProduct(Product(3, "Buk", 20.0));
+  s.addProduct(Product(4, "Leng", 25.0));
+
+  s.removeById(2);
+
+  List<Product> results = s.inPriceRange(10.0, 25.0);
+  for (var p in results) {
+    print("#${p.id} ${p.name} - ${p.price.toStringAsFixed(2)}");
+  }
 }
