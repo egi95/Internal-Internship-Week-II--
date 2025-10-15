b3cb25a928667a74beed199e4ec805c79f4db38e
Oltioni162 <oc198@bgt.school>
2025-10-15 15:43:53 +0200

    Create Task1.dart

---
+++ Day3/Task1.dart
@@ -0,0 +1,25 @@
+Task 1 – Buton që ndryshon tekstin
+
+Përshkrimi
+
+Krijoni një aplikacion Flutter Web me `MaterialApp` dhe `Scaffold`.
+
+Në `home`, vendosni një buton që me `setState()`:
+
+- ndërron tekstin nga "Përshëndetje" në "U përditësua!" (ose anasjelltas),
+
+- ose rrit një numërues (counter).
+
+Shtoni një `SnackBar` që shfaqet kur kryhet veprimi.
+
+Udhëzime për testim / Output i pritur
+
+• Aplikacioni duhet të nisë me `flutter run -d chrome` pa gabime të kuqe.
+
+• Në klikim, UI duhet të përditësohet dhe të shfaqet SnackBar.
+
+Udhëzime për dorëzim
+
+Detyrën e përfunduar duhet ta vendosni në branch-in tuaj personal në Git, brenda folderit të Day3.
+
+Ruajeni skedarin me një emër të qartë (p.sh. task1.dart, task2.dart, ose flutter_app_day3/) dhe bëni commit & push pasi ta keni testuar që funksionon pa gabime.
