a42016ff6401902ef49e2f7c85455f13c287eb02
Adriatik112 <162362024+Adriatik112@users.noreply.github.com>
2025-10-15 14:21:36 +0100

    Create Task1.dart

---
+++ Day3/Task1.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Day3',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePag
