c753fa3619df6cd3187453a34e276992e04f116e
anisarat <ar129@bgt.school>
2025-10-15 14:05:00 +0200

    Task2.dart

---
+++ Day3/Task2.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lista me shtim dhe fshirje',
+      home: ItemListScreen(),
+    );
+  }
+}
+
+
+class ItemListScreen extends StatefulWidget {
+  @override
+  _ItemListScreenState createState() => _ItemListScreenState();
+}
+
+class _ItemListScreenState extends State<ItemListScreen> {
+
+  List<String> items = [];
+
+  
+  final TextEditingController _controller = TextEditingController();
+
+ 
+  void addItem() {
+    final text = _controller.text.trim();
+    if (text.isNotEmpty) {
+      setState(() {
+        items.add(text);
+        _controller.clear();
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Artikulli "$text" u shtua!')),
+      );
+    }
+  }
+
+
+  void deleteItem(int index) {
+    final deletedItem = items[index];
+    setState(() {
+      items.removeAt(index);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Artikulli "$deletedItem" u fshi!')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listë me shtim dhe fshirje'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+           
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      labelText: 'Shkruaj artikullin',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: addItem,
+                  child: Text('Shto'),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(items[index]),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete, color: Colors.red),
+                        onPressed: () => deleteItem(index),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
