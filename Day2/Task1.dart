2ca307f5607aa4ba492f3d076456a32f6a8446a4
Vesahalimi <vh423@bgt.school>
2025-10-16 09:13:59 +0200

    Update Task1.dart

--- Day2/Task1.dart
+++ Day2/Task1.dart
@@ -1,1 +1,29 @@
+class Student{
+  String emri;
+  List<double>nota;
 
+  Student(this.emri, this.nota);
+
+  double mesatarja() {
+    if (nota.isEmpty) return 0.0;
+    double sum = nota.reduce((a, b) => a + b);
+    return sum / nota.length;
+  }
+}
+
+void main() { 
+  var students = [
+    Student ("Ana", [8,9,10])
+    Student ("Ardit", [6,7,8])
+    Student ("Besa", [])
+  ];
+
+  Student top = students[0];
+  for (var s in students) {
+    if (s.mesatarja() > top.mesatarja()) { 
+      top = s;
+    } 
+  } 
+
+  print("Top: ${top.emri} -${top.mesatarja().toStringAsFixed(2)}");
+}
