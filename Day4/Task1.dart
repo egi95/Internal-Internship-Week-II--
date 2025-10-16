869fd1aa706972d70b53c3facba18cb3ad97c843
aron1i <ar137@bgt.school>
2025-10-16 19:06:20 +0200

    Update Task1.dart

--- Day4/Task1.dart
+++ Day4/Task1.dart
@@ -1,1 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: UserListPage(),
+    );
+  }
+}
+
+class UserListPage extends StatefulWidget {
+  const UserListPage({super.key});
+
+  @override
+  State<UserListPage> createState() => _UserListPageState();
+}
+
+class _UserListPageState extends State<UserListPage> {
+  late Future<List<dynamic>> usersFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    usersFuture = getUsers();
+  }
+
+  Future<List<dynamic>> getUsers() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/users'),
+    );
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Gabim gjatë ngarkimit të përdoruesve');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Përdoruesit')),
+      body: FutureBuilder<List<dynamic>>(
+        future: usersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Gabim: ${snapshot.error}')),
+              );
+            });
+            return const Center(child: Text('Nuk u ngarkuan të dhënat.'));
+          } else if (snapshot.hasData) {
+            
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  title: Text(user['name']),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPage(user: user),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          } else {
+            return const Center(child: Text('Asnjë e dhënë.'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final dynamic user;
+  const DetailPage({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(user['name'])),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Emër: ${user['name']}'),
+            Text('Email: ${user['email']}'),
+            Text('Telefon: ${user['phone']}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
