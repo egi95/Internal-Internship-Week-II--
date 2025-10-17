9b806a80f5a078b8be6396a32155d5954d0dcc45
orgescakolli <oc117@bgt.school>
2025-10-17 13:49:49 +0200

    Create main.dart

---
+++ Day5/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Library App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
