e2b35dc85d190ca1ceaf8c6d1e282774d89cb363
alkiiiiiiiiiiiiiiiiiiiiiiiiiiiii <ai162@bgt.school>
2025-10-17 08:42:01 +0200

    Update Task1.dart

--- Day2/Task1.dart
+++ Day2/Task1.dart
@@ -1,1 +1,30 @@
+class Student {
+  String emri;
+  List<double> nota;
 
+  Student(this.emri, this.nota);
+
+  double mesatarja() {
+    if (nota.isEmpty) return 0.0;
+    double shuma = nota.reduce((a, b) => a + b);
+    return shuma / nota.length;
+  }
+}
+
+void main() {
+  List<Student> studentet = [
+    Student('Guri', [8, 9, 10]),
+    Student('Lisi', [6, 7, 8]),
+    Student('Vlera', []),
+    Student('Brikena', [9, 10, 10])
+  ];
+
+  Student topStudent = studentet[0];
+  for (var s in studentet) {
+    if (s.mesatarja() > topStudent.mesatarja()) {
+      topStudent = s;
+    }
+  }
+
+  print('Top: ${topStudent.emri} - ${topStudent.mesatarja().toStringAsFixed(2)}');
+}
