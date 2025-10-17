141da098fdd965f6a3e29ad5772e173b6ed80f5c
Olt Istogu <oi122@bgt.school>
2025-10-17 08:37:16 +0200

    Create a Flutter app with greeting and counter

--- Day3/Task1.dart
+++ Day3/Task1.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isGreeting = true;
+  int _counter = 0;
+
+  void _onPressed() {
+    setState(() {
+      _isGreeting = !_isGreeting;
+      _counter++;
+    });
+    
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Veprim i kryer! $_counter')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Web')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_isGreeting ? 'Përshëndetje' : 'U përditësua!'),
+            Text('Counter: $_counter'),
+            ElevatedButton(
+              onPressed: _onPressed,
+              child: Text('Kliko'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
