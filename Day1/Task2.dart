67e8a55c249592bc2e783a767d09ab7c273996c3
Vesahalimi <vh423@bgt.school>
2025-10-19 13:22:08 +0200

    Update Task2.dart

--- Day1/Task2.dart
+++ Day1/Task2.dart
@@ -1,1 +1,34 @@
+void main () { 
+  // Shembull inputi 
+  List<int> numrat = [1,2,3,4,5,6,];
+  Map<String, List<int>> resultati = ndaj (numrat);
+
+  // Printimi i rezultateve 
+  print("Numrat cift: ${rezultati ['cift']}");     
+  print("Numrat tek:  ${rezultati ['tek']}");
+} 
+
+// Funksioni qe ndan numrat ne cift dhe tek 
+Map<String, list<int>> nadaj(List<int> nums) {
+  List<int> cift = [];
+  list<int> tek = [];
+
+  // Kalimi neper secilin numer 
+  for (int n in nums) { 
+    if (n % 2 == 0) {
+      cift.add(n);
+    } else { 
+      tek.add(n);
+    }
+  }
+
+  // Kthejme rezultatet si harte (Map)
+  return {
+    'cift': cift,
+    'tek': tek,
+  };
+}
+
+
+
 
